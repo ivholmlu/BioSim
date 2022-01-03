5329676a257cf12407642388bd573ca6ec8b4de3
Ivar Holmlund <ivholmlu@nmbu.no>
2022-01-03 14:01:18 +0100

    Added herbivores and carvinores dictionaries with parameters to __innit__

--- src/biosim/simulation.py
+++ src/biosim/simulation.py
@@ -12,6 +12,19 @@ class BioSim:
                  img_dir=None, img_base=None, img_fmt='png', img_years=None,
                  log_file=None):
 
+        herb = {'w_birth' : 8.0, 'sigma_birth' : 1.5, 'beta' : 0.9,
+                'eta' : 0.05, 'a_half' : 40.0, 'phi_age' : 0.6,
+                'w-half' : 10.0, 'phi_weight' : 0.1, 'mu' : 0.25,
+                'gamma' : 0.2, 'zeta' : 3.5, 'xi': 1.2,
+                'omega' : 0.4, 'F' : 10.0, 'DeltaPhiMax' : None}
+
+        carn = {'w_birth' : 6.0, 'sigma_birth' : 1.0, 'beta' : 0.75,
+                'eta' : 0.125, 'a_half' : 40.0, 'phi_age' : 0.3,
+                'w-half' : 4.0, 'phi_weight' : 0.4, 'mu' : 0.4,
+                'gamma' : 0.8, 'zeta' : 3.5, 'xi': 1.1,
+                'omega' : 0.8, 'F' : 50.0, 'DeltaPhiMax' : 10.0}
+
+
         """
         :param island_map: Multi-line string specifying island geography
         :param ini_pop: List of dictionaries specifying initial population
@@ -53,6 +66,7 @@ class BioSim:
         :param species: String, name of animal species
         :param params: Dict with valid parameter specification for species
         """
+
 
     def set_landscape_parameters(self, landscape, params):
         """
