02da882a726b4e430fa466823131453ccc4dac64
Ivar Holmlund <ivholmlu@nmbu.no>
2022-01-04 11:32:58 +0100

    Removed herb and car dict from simulation.py. Created module for herbivores

---
+++ src/biosim/Herbivores.py
@@ -0,0 +1,20 @@
+"""
+Class for herbivores. Will later on be merged into class for animals.
+There will herbivores be a subclass
+herb = {'w_birth' : 8.0, 'sigma_birth' : 1.5, 'beta' : 0.9,
+                'eta' : 0.05, 'a_half' : 40.0, 'phi_age' : 0.6,
+                'w-half' : 10.0, 'phi_weight' : 0.1, 'mu' : 0.25,
+                'gamma' : 0.2, 'zeta' : 3.5, 'xi': 1.2,
+                'omega' : 0.4, 'F' : 10.0, 'DeltaPhiMax' : None}
+
+        carn = {'w_birth' : 6.0, 'sigma_birth' : 1.0, 'beta' : 0.75,
+                'eta' : 0.125, 'a_half' : 40.0, 'phi_age' : 0.3,
+                'w-half' : 4.0, 'phi_weight' : 0.4, 'mu' : 0.4,
+                'gamma' : 0.8, 'zeta' : 3.5, 'xi': 1.1,
+                'omega' : 0.8, 'F' : 50.0, 'DeltaPhiMax' : 10.0}
+"""
+
+
+class Herbivores:
+
+    pass
