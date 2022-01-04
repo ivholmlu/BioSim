5f4e1cc94483a8c58319f990d0222a8774504d9e
Aaron De Leyos <aaron.leyds.mangaron.de.leyos@nmbu.no>
2022-01-04 12:49:09 +0100

    Added a Lowland-class describing the conditions of the landscape.

--- src/biosim/landscape.py
+++ src/biosim/landscape.py
@@ -2,11 +2,18 @@
     #  pass
 
 class Lowland:  # senere Lowland(Landscape):
-    f_max = 300.0
+    def __init__(self, f_max=300, list_animals=None):
+        self.f_max = f_max
+        self.list_animals = list_animals
+        self.current_fodder = self.f_max
+        self.n_animals = len(self.list_animals)
 
-    def avail_fodder(self):  # fordeler mat
+    def avail_fodder(self, n_animals):  # fordeler mat
+        for animal in n_animals:
+            pass
         pass
 
     def replenish(self):  # fyller på mat for hvert år som går
+        current_fodder = self.current_fodder
         pass
 
