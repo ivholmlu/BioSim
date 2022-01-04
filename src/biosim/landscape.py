fe8365f97336296a80d26d0acf132e92f678e87f
Aaron De Leyos <aaron.leyds.mangaron.de.leyos@nmbu.no>
2022-01-04 11:49:39 +0100

    Added landscape.py module in the biosim package.

---
+++ src/biosim/landscape.py
@@ -0,0 +1,12 @@
+# class Landscape:
+    #  pass
+
+class Lowland:  # senere Lowland(Landscape):
+    f_max = 300.0
+
+    def avail_fodder(self):  # fordeler mat
+        pass
+
+    def replenish(self):  # fyller på mat for hvert år som går
+        pass
+
