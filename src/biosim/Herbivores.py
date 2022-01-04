475c0f01cc259a6267c36447a73d4b5997d1bcec
Aaron De Leyos <aaron.leyds.mangaron.de.leyos@nmbu.no>
2022-01-04 15:10:56 +0100

    Changed variable name from herb to param to make it more general for future use.

--- src/biosim/Herbivores.py
+++ src/biosim/Herbivores.py
@@ -8,8 +8,7 @@ import math
 import random
 
 class Herbivores:
-
-    herb = {'w_birth': 8.0, 'sigma_birth': 1.5, 'beta': 0.9,
+    param = {'w_birth': 8.0, 'sigma_birth': 1.5, 'beta': 0.9,
             'eta': 0.05, 'a_half': 40.0, 'phi_age': 0.6,
             'w-half': 10.0, 'phi_weight': 0.1, 'mu': 0.25,
             'gamma': 0.2, 'zeta': 3.5, 'xi': 1.2,
@@ -25,25 +24,25 @@ class Herbivores:
         self.age += 1
         return self.age
 
-    def weight_flux(self, gain):
-        self.weight += herb['beta'] * gain
+    def weight_flux(self, beta=param['beta'], gain=0):
+        self.weight += beta * gain
         return self.weight
 
-    def weight_loss(self):
-        self.weight -= self.weight * herb['eta']
+    def weight_loss(self, eta=param['eta']):
+        self.weight -= self.weight * eta
 
-    def fitness_flux(self):
-        q_age = 1 / (1 + math.exp(herb['phi'] * (self.age - herb['a_half'])))
-        q_weight = 1 / (1 + math.exp(-herb['phi'] * (self.weight - herb['w_half'])))
+    def fitness_flux(self, phi=param['phi'], a_half=param['a_half'], w_half=param['w_half']):
+        q_age = 1 / (1 + math.exp(phi * (self.age - a_half)))
+        q_weight = 1 / (1 + math.exp(-phi * (self.weight - w_half)))
         self.fitness = q_age * q_weight
         return self.fitness
 
-    def death(self):
+    def death(self, omega=param['omega']):
         if self.weight == 0:
             self.alive = False
 
         else:
-            p = herb['omega'] * (1 - self.fitness)
+            p = omega * (1 - self.fitness)
             a = random.random() # husk å bytte navn på variabelen
             if a < p:
                 self.alive = False
