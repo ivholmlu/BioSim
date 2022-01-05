d6b2caa41c583a88090ec03b2707376c50645546
Aaron De Leyos <aaron.leyds.mangaron.de.leyos@nmbu.no>
2022-01-05 12:37:20 +0100

    Changed name for method avail_fodder to feed. Changed order of operations in the method feed. Corrected replenish method as it now resets to f_max.

--- src/biosim/landscape.py
+++ src/biosim/landscape.py
@@ -4,27 +4,24 @@
 import Herbivores
 
 class Lowland:  # senere Lowland(Landscape):
-    def __init__(self, f_max=300, list_animals=None): # vha. set_lanscape_parameters så skal vi få inn f_max.
+    def __init__(self, list_animals=None, f_max=300): # vha. set_lanscape_parameters så skal vi få inn f_max.
         self.f_max = f_max
         self.list_animals = list_animals
         self.current_fodder = self.f_max
-        self.n_animals = len(self.list_animals)
 
-    def avail_fodder(self):  # fordeler mat
+    def feed(self):  # fordeler mat, mater faktisk dyrene.
         for animal in self.list_animals:
             if self.current_fodder >= animal.param['F']:
+                animal.weight_gain(animal.param['F'])
                 self.current_fodder -= animal.param['F']
-                animal.weight_flux(animal.param['F'])
 
             elif self.current_fodder < animal.param['F']:
+                animal.weight_gain(self.current_fodder)
                 self.current_fodder -= self.current_fodder
-                animal.weight_flux(self.current_fodder)
 
-            elif self.current_fodder <= 0:
+            elif self.current_fodder == 0:
                 break
 
 
     def replenish(self):  # fyller på mat for hvert år som går, kan være i overklassen.
-        current_fodder = self.current_fodder
-        pass
-
+        self.current_fodder = self.f_max
