47d9115c721206b1d1355fc4dcc5c15088499e56
Ivar Holmlund <ivholmlu@nmbu.no>
2022-01-05 13:34:59 +0100

    Created test_herbivores_creation()

--- tests/test_animals.py
+++ tests/test_animals.py
@@ -1,13 +1,28 @@
 from biosim.animals import Herbivores
-test_animal = {'age' : 0, 'weight' : 10}
+test_animal = {'age' : 0, 'weight' : 10,}
 
-def test_aging():
 
-    num_years = 10
-    animal = Herbivores(test_animal)
-    for _ in range(num_years):
-        animal.aging()
-    assert animal.age == num_years
+
+class test_aging_dicision():
+
+#    @pytest.fixture(autouse = True)
+#    def create_Herb(self):
+
+    def test_aging():
+
+        num_years = 10
+        animal = Herbivores(test_animal)
+        for _ in range(num_years):
+            animal.aging()
+        assert animal.age == num_years
+
+    def test_herbivores_creation():
+        """
+        Creation has age set to 0
+        """
+        Herb = Herbivores()
+        assert Herb.age == 0
+
 
 
 
