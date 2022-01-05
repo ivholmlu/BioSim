969c0747e3696cce61e1180c4dfb8e6bf1cfa869
Aaron De Leyos <aaron.leyds.mangaron.de.leyos@nmbu.no>
2022-01-05 20:31:36 +0100

    Further improved the Lowland class. Added a new attribute self.population, added new methods add_population, procreate, aging, weight_cut and deceased. The new methods builds further on top of animals.py's Herbivore class.

--- src/biosim/landscape.py
+++ src/biosim/landscape.py
@@ -1,12 +1,18 @@
 from .animals import Herbivores
-# class Landscape:
-#pass
 
 class Lowland:  # senere Lowland(Landscape):
     def __init__(self, list_animals=None, f_max=300): # vha. set_lanscape_parameters så skal vi få inn f_max.
         self.f_max = f_max
         self.list_animals = list_animals
         self.current_fodder = self.f_max
+        self.population = None
+
+    def add_population(self):
+        pop = self.list_animals[0]['pop']
+        self.population = [Herbivores(animal) for animal in pop]
+
+    def init_fitness(self):
+        fitness0 = [animal.fitnes_flux() for animal in self.population]
 
     def sort_fitness(self):
         self.list_animals.sort(key=lambda animal: animal.fitness, reverse=True)
@@ -24,6 +30,26 @@ class Lowland:  # senere Lowland(Landscape):
             elif self.current_fodder == 0:
                 break
 
+    def procreate(self):
+        num_pop = len(self.population)
+        eligibility_to_procreate = [animal.birth()
+                                    for animal in self.population]
+        spawned = [self.population.append(Herbivores(animal.baby))
+                   for animal in self.population if animal.give_birth is True]
+
+    def aging(self):
+        for animal in self.population:
+            animal.ages()
+
+    def weight_cut(self):
+        new_weight = [self.population.weight_loss(animal) for animal in
+                    self.population if animal.alive is False]
+
+    def deceased(self):
+        for animal in self.population:
+            animal.death()
+        perished = [self.population.remove(animal) for animal in
+                    self.population if animal.alive is False]
 
     def replenish(self):  # fyller på mat for hvert år som går, kan være i overklassen.
         self.current_fodder = self.f_max
