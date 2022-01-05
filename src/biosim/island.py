be8a2a6008cb6dc06764b8c23fe9a7d40f14efe2
Aaron De Leyos <aaron.leyds.mangaron.de.leyos@nmbu.no>
2022-01-05 20:37:13 +0100

    Created a new file island.py containing a class Island. The Island class builds further upon the Lowland and Herbivores class

---
+++ src/biosim/island.py
@@ -0,0 +1,20 @@
+from .landscape import Lowland
+
+class Island():
+    def __init__(self, list_of_animals=None):
+        self.island = Lowland(list_of_animals)
+
+    # Én syklus for ett landområde
+    def cycle(self):
+        # Kan videre forbedres ved å implementere .sort_fitness() i hver av funksjonene
+        self.island.init_fitness()
+        self.island.feed()
+        self.island.sort_fitness()
+        self.island.procreate()
+        self.island.aging()
+        self.island.sort_fitness()
+        self.island.weight_cut()
+        self.island.sort_fitness()
+        self.island.deceased()
+        self.island.replenish()
+
