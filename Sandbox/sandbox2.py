f8f2d32f9569b0d095d4024694e6a13d68b125b5
Aaron De Leyos <aaron.leyds.mangaron.de.leyos@nmbu.no>
2022-01-06 17:39:18 +0100

    Implemented a skeleton for the simulation with the Island class.

--- Sandbox/sandbox2.py
+++ Sandbox/sandbox2.py
@@ -1,24 +1,42 @@
 #%%
 import random
 from biosim.landscape import Lowland
-random.seed(123)
+from biosim.island import Island
+
+random.seed(61361137)
 
 ini_herbs = [{'pop': [{'species': 'Herbivore',
                        'age': 5,
                        'weight': 20}
                       for _ in range(50)]}]
+c = Island(ini_herbs)
 
-a = Lowland(ini_herbs, 378)
-a.add_population()
+for _ in range(200):
+    a.cycle()
 
-for _ in range(50):
-    a.init_fitness()
-    a.sort_fitness()
-    a.feed()
-    a.procreate()
-    a.aging()
-    a.weight_cut()
-    a.deceased()
-    a.replenish()
 
-print(len(a.population))
+
+#%%
+n = 0
+for i, j in enumerate(a.population, 1):
+    print(i, j.fitness)
+
+print(n)
+
+#%%
+ini_herbs = [{'pop': [{'species': 'Herbivore',
+                       'age': 5,
+                       'weight': 20}
+                      for _ in range(50)]}]
+c = Lowland(ini_herbs)
+
+for _ in range(200):
+    c.init_fitness()
+    c.sort_fitness()
+    c.feed()
+    c.procreate()
+    c.aging()
+    c.weight_cut()
+    c.deceased()
+    c.replenish()
+print(len(c.population))
