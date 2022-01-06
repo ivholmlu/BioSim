4f97a0827bffd4fbca0a7880d70a2ebfeab0a937
Ivar Holmlund <ivholmlu@nmbu.no>
2022-01-06 21:01:43 +0100

    Added simple vizualisation to sandbox2 file

--- Sandbox/sandbox2.py
+++ Sandbox/sandbox2.py
@@ -1,17 +1,20 @@
 #%%
 import random
 from biosim.landscape import Lowland
-random.seed(123)
+import matplotlib.pyplot as plt
+random.seed(100)
 
 ini_herbs = [{'pop': [{'species': 'Herbivore',
                        'age': 5,
                        'weight': 20}
                       for _ in range(50)]}]
 
-a = Lowland(ini_herbs, 378)
+a = Lowland(ini_herbs)
 a.add_population()
-
-for _ in range(50):
+years = []
+years_pop = []
+years_babies = []
+for _ in range(100):
     a.init_fitness()
     a.sort_fitness()
     a.feed()
@@ -20,5 +23,16 @@ for _ in range(50):
     a.weight_cut()
     a.deceased()
     a.replenish()
+    years.append(_)
+    years_pop.append(len(a.population))
+    years_babies.append(len(a.babies))
 
+plt.plot(years, years_pop)
+plt.plot(years, years_babies)
+plt.title('Amount and birth each year')
+plt.xlabel('year')
+plt.ylabel('amount')
+plt.legend(['weight', 'birth'])
+plt.show()
 print(len(a.population))
+
