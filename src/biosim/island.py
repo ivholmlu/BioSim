98093f9a8f9f13b30e0d16450712224ac5f1a81a
Aaron De Leyos <aaron.leyds.mangaron.de.leyos@nmbu.no>
2022-01-06 17:41:11 +0100

    Changed attribute name from island to tiles.

--- src/biosim/island.py
+++ src/biosim/island.py
@@ -2,17 +2,17 @@ from .landscape import Lowland
 
 class Island():
     def __init__(self, list_of_animals=None):
-        self.island = Lowland(list_of_animals)
+        self.tiles = Lowland(list_of_animals)
 
     # Én syklus for ett landområde
     def cycle(self):
         # Kan videre forbedres ved å implementere .sort_fitness() i hver av funksjonene
-        self.island.init_fitness()
-        self.island.sort_fitness()
-        self.island.feed()
-        self.island.procreate()
-        self.island.aging()
-        self.island.weight_cut()
-        self.island.deceased()
-        self.island.replenish()
+        self.tiles.init_fitness()
+        self.tiles.sort_fitness()
+        self.tiles.feed()
+        self.tiles.procreate()
+        self.tiles.aging()
+        self.tiles.weight_cut()
+        self.tiles.deceased()
+        self.tiles.replenish()
 
