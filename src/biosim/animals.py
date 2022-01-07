4dffb0ae09fcebb59b5967b121b3046c9e4f85ff
Ivar Holmlund <ivholmlu@nmbu.no>
2022-01-07 12:39:53 +0100

    Made carnivores and herbivores subclasses of animals

--- src/biosim/animals.py
+++ src/biosim/animals.py
@@ -5,13 +5,13 @@ There will herbivores be a subclass
 import math
 import random
 
+class animals:
 
-class Herbivores:
     param = {'w_birth': 8.0, 'sigma_birth': 1.5, 'beta': 0.9,
-             'eta': 0.05, 'a_half': 40.0, 'phi_age': 0.6,
-             'w_half': 10.0, 'phi_weight': 0.1, 'mu': 0.25,
-             'gamma': 0.2, 'zeta': 3.5, 'xi': 1.2,
-             'omega': 0.4, 'F': 10.0, 'DeltaPhiMax': None}
+            'eta': 0.05, 'a_half': 40.0, 'phi_age': 0.6,
+            'w_half': 10.0, 'phi_weight': 0.1, 'mu': 0.25,
+            'gamma': 0.2, 'zeta': 3.5, 'xi': 1.2,
+            'omega': 0.4, 'F': 10.0, 'DeltaPhiMax': None}
 
     def __init__(self, dict=None):
         self.age = dict['age']
@@ -19,17 +19,6 @@ class Herbivores:
         self.fitness = 0
         self.alive = True
         self.baby = {'age': 0, 'weight': 0.0}
-
-# Vi får bekymre oss for dette på et senere tidspunkt
-#@classmethod
-    #def set_params(cls, new_params):
-     #   for key in new_params.keys():
-      #      if key not in new_params.keys():
-       #         raise KeyError('Invalid parameter name: ' + key)
-        #    else:
-         #       cls.param[key] = new_params[key]
-
-        #return cls.param
 
     def fitness_flux(self, a_half=param['a_half'], w_half=param['w_half'],
                      phi_age=param['phi_age'], phi_weight=param['phi_weight']):
@@ -76,3 +65,39 @@ class Herbivores:
             p = omega * (1 - self.fitness)
             if random.random() < p:
                 self.alive = False
+
+class Herbivores(animals):
+
+    def __init__(self):
+
+        self.param = {'w_birth': 8.0, 'sigma_birth': 1.5, 'beta': 0.9,
+                 'eta': 0.05, 'a_half': 40.0, 'phi_age': 0.6,
+                 'w_half': 10.0, 'phi_weight': 0.1, 'mu': 0.25,
+                 'gamma': 0.2, 'zeta': 3.5, 'xi': 1.2,
+                 'omega': 0.4, 'F': 10.0, 'DeltaPhiMax': None}
+
+
+class Carnivores(animals):
+
+    def __init__(self):
+
+        self.param = {'w_birth': 8.0, 'sigma_birth': 1.5, 'beta': 0.9,
+                 'eta': 0.05, 'a_half': 40.0, 'phi_age': 0.6,
+                 'w_half': 10.0, 'phi_weight': 0.1, 'mu': 0.25,
+                 'gamma': 0.2, 'zeta': 3.5, 'xi': 1.2,
+                 'omega': 0.4, 'F': 10.0, 'DeltaPhiMax': None}
+
+
+
+
+
+# Vi får bekymre oss for dette på et senere tidspunkt
+#@classmethod
+    #def set_params(cls, new_params):
+     #   for key in new_params.keys():
+      #      if key not in new_params.keys():
+       #         raise KeyError('Invalid parameter name: ' + key)
+        #    else:
+         #       cls.param[key] = new_params[key]
+
+        #return cls.param
