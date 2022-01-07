0ec5c44b6b95133f46ce35b28cc812db133ae616
Aaron De Leyos <aaron.leyds.mangaron.de.leyos@nmbu.no>
2022-01-07 16:24:19 +0100

    Reformatted the feed method.

--- src/biosim/landscape.py
+++ src/biosim/landscape.py
@@ -1,5 +1,6 @@
 from .animals import Herbivores, Carnivores
 import itertools
+import random
 
 class Lowland:  # senere Lowland(Landscape):
     def __init__(self, list_animals=None, f_max=800):  # vha. set_lanscape_parameters så skal vi få inn f_max.
@@ -15,7 +16,7 @@ class Lowland:  # senere Lowland(Landscape):
 
     def add_population(self):
         for animal in self.pop:
-            if animal['species']== 'Herbivore':
+            if animal['species'] == 'Herbivore':
                 self.herbivores.append(Herbivores(animal))
             else:
                 self.carnivores.append(Carnivores(animal))
@@ -24,46 +25,45 @@ class Lowland:  # senere Lowland(Landscape):
         #        else self.carnivores.append(Carnivores(x)) for x in self.pop]
 
     def init_fitness(self):
-        fitness0 = [animal.fitness_flux() for animal in self.population]
+        fitness0 = [animal.fitness_flux() for animal in self.carnivores+self.herbivores]
 
     def sort_fitness(self):
-        self.population.sort(key=lambda animal: animal.fitness, reverse=True)
+        self.herbivores.sort(key=lambda animal: animal.fitness, reverse=True)
+        self.carnivores.sort(key=lambda animal: animal.fitness, reverse=True)
 
     def feed(self):  # fordeler mat, mater faktisk dyrene.
-        for animal in self.population:
-            if animal.species == 'Herbivore':
-                if self.current_fodder >= animal.param['F']:
-                    animal.weight_gain(animal.param['F'])
-                    self.current_fodder -= animal.param['F']
+        for herbivore in self.herbivores:
+            if herbivore.species == 'Herbivore':
+                if self.current_fodder >= herbivore.param['F']:
+                    herbivore.weight_gain(herbivore.param['F'])
+                    self.current_fodder -= herbivore.param['F']
 
-                elif self.current_fodder < animal.param['F']:
-                    animal.weight_gain(self.current_fodder)
+                elif self.current_fodder < herbivore.param['F']:
+                    herbivore.weight_gain(self.current_fodder)
                     self.current_fodder -= self.current_fodder
 
                 elif self.current_fodder == 0:
                     break
 
-            else:
-                for carnivore in self.carnivores:
-                    phi_carn = carnivore.fitness
-                    delta_phi_max = carnivore.param['DeltaPhiMax']
+        for carnivore in self.carnivores:
+            phi_carn = carnivore.fitness
+            delta_phi_max = carnivore.param['DeltaPhiMax']
 
-                    while carnivore.fed < carnivore.param['F']:
-                        for herbivore in self.herbivores:
-                            phi_herb = herbivore.fitness
-                            if 0 < phi_carn - phi_herb < delta_phi_max:
-                                p = phi_carn - phi_herb / delta_phi_max
-                                if random.random() < p:
-                                    herbivore.alive = False
-                                    carnivore.fed += herbivore.weight
-                            elif phi_carn - phi_herb > delta_phi_max:
-                                herbivore.alive = False
-                                carnivore.fed += herbivore.weight
+            while carnivore.fed < carnivore.param['F']:
+                for herbivore in self.herbivores:
+                    phi_herb = herbivore.fitness
+                    if 0 < phi_carn - phi_herb < delta_phi_max:
+                        p = phi_carn - phi_herb / delta_phi_max
+                        if random.random() < p:
+                            herbivore.alive = False
+                            carnivore.fed += herbivore.weight
+                            carnivore.weight_gain(carnivore.fed)
+                    elif phi_carn - phi_herb > delta_phi_max:
+                        herbivore.alive = False
+                        carnivore.fed += herbivore.weight
+                        carnivore.weight_gain(carnivore.fed)
 
-
-
-                pass
-
+            carnivore.weight_gain(carnivore.fed)
 
     def procreate(self):
         num_pop_herb = len(self.herbivores)
