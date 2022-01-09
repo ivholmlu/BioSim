98e9eef9f24be29e3e782794c993b831ebe79e99
Aaron De Leyos <aaron.leyds.mangaron.de.leyos@nmbu.no>
2022-01-09 15:13:22 +0100

    Added additional changes to the different methods.

--- src/biosim/landscape.py
+++ src/biosim/landscape.py
@@ -2,6 +2,7 @@ from .animals import Herbivores, Carnivores
 import itertools
 import random
 
+
 class Lowland:  # senere Lowland(Landscape):
     def __init__(self, list_animals=None, f_max=800):  # vha. set_lanscape_parameters så skal vi få inn f_max.
         self.f_max = f_max
@@ -10,28 +11,28 @@ class Lowland:  # senere Lowland(Landscape):
         self.current_fodder = self.f_max
         self.carnivores = []
         self.herbivores = []
-        self.babies = None
 
-        # må finne bedre måte å implementere dyrebestanden inn
+    def replenish(self):  # fyller på mat for hvert år som går, kan være i overklassen.
+        self.current_fodder = self.f_max
 
-    def add_population(self):
-        for animal in self.pop:
-            if animal['species'] == 'Herbivore':
-                self.herbivores.append(Herbivores(animal))
-            else:
-                self.carnivores.append(Carnivores(animal))
-
-        # z = [self.herbivores.append(Herbivores(x)) if x.species == 'Herbivore'
-        #        else self.carnivores.append(Carnivores(x)) for x in self.pop]
+    def append_population(self, ext_population=None):
+        if ext_population is None:
+            init_pop = [self.herbivores.append(Herbivores(animal)) if animal['species'] == 'Herbivore'
+                        else self.carnivores.append(Carnivores(animal)) for animal in self.pop]
+        else:
+            nu_pop = ext_population[0]['pop']
+            append_pop = [self.herbivores.append(Herbivores(animal)) if animal['species'] == 'Herbivore'
+                          else self.carnivores.append(Carnivores(animal)) for animal in nu_pop]
 
     def init_fitness(self):
         fitness0 = [animal.fitness_flux() for animal in self.carnivores+self.herbivores]
 
     def sort_fitness(self):
         self.herbivores.sort(key=lambda animal: animal.fitness, reverse=True)
-        self.carnivores.sort(key=lambda animal: animal.fitness, reverse=True)
+        # Vi sorterer kun herbivores ut ifra fitness
 
-    def feed(self):  # fordeler mat, mater faktisk dyrene.
+    def feed(self):  # Rewrite the feeding method. Perhaps make two separate feeding methods for each type?
+        # At least rewrite the carnivore feeding logic.
         for herbivore in self.herbivores:
             if herbivore.species == 'Herbivore':
                 if self.current_fodder >= herbivore.param['F']:
@@ -44,6 +45,7 @@ class Lowland:  # senere Lowland(Landscape):
 
                 elif self.current_fodder == 0:
                     break
+        self.herbivores.sort(key=lambda animal: animal.fitness, reverse=False)
 
         for carnivore in self.carnivores:
             phi_carn = carnivore.fitness
@@ -69,27 +71,27 @@ class Lowland:  # senere Lowland(Landscape):
         num_pop_herb = len(self.herbivores)
         num_pop_carn = len(self.carnivores)
         #Herbivores
-        self.babies = [baby for parent in self.herbivores if (baby := parent.birth(num_pop_herb))]
-        self.herbivores += self.babies
+        baby_herb = [baby for parent in self.herbivores if (baby := parent.birth(num_pop_herb))]
+        self.herbivores += baby_herb
+
         #Carnivores
-        self.babies = [baby for parent in self.carnivores if(baby := parent.birth(num_pop_carn))]
-        self.carnivores += self.babies
+        baby_carn = [baby for parent in self.carnivores if(baby := parent.birth(num_pop_carn))]
+        self.carnivores += baby_carn
 
     def aging(self):
-        for animal in self.population:
+        for animal in itertools.chain(self.carnivores, self.herbivores):
             animal.ages()
 
     def weight_cut(self):
-        new_weight = [animal.weight_loss() for animal in self.population]
+        new_weight = [animal.weight_loss() for animal in itertools.chain(self.carnivores, self.herbivores)]
 
     def deceased(self):
-        for animal in self.population:
+        for animal in itertools.chain(self.carnivores, self.herbivores):
             animal.death()
-        self.population = [animal for animal in self.population if animal.alive
+        self.carnivores = [carnivore for carnivore in self.carnivores if carnivore.alive
+                           is True]
+        self.herbivores = [herbivore for herbivore in self.herbivores if herbivore.alive
                            is True]
 
-    def replenish(self):  # fyller på mat for hvert år som går, kan være i overklassen.
-        self.current_fodder = self.f_max
-
     def get_population(self):
-        return len(self.population)
+        return len(self.carnivores+self.herbivores)
