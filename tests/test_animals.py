de493a151b904c677b6902ba11dea53b8e1af3ba
Ivar Holmlund <ivholmlu@nmbu.no>
2022-01-09 21:56:07 +0100

    Rewrote and parametrizised test for aging function

--- tests/test_animals.py
+++ tests/test_animals.py
@@ -1,21 +1,42 @@
 import pytest
 from biosim.animals import Herbivores
 
-test_animal = {'age': 40, 'weight': 25}
+
+test_animal = {'age': 40, 'weight': 25, 'species' : 'Herbivore'}
 
 
-def test_aging():
-    num_years = 10
+@pytest.mark.parametrize("age, num_years, expected_age",
+                         [(40, 1, 41),
+                          (0, 3, 3)])
+#Sliter med å få dictionary inn i parametriseringen.
+#Funket ikke å skrive inn med dictionary selv om man bruke testclass
+def test_aging(age, num_years, expected_age):
+    test_animal['age'] = age
     animal = Herbivores(test_animal)
     for _ in range(num_years):
         animal.ages()
-    assert animal.age == num_years + test_animal['age']
+    assert animal.age == expected_age
 
 
+
+
+"""
+@pytest.mark.parametrize('test_animal, num_years, expected',
+                        [('test_animal1', 10, 50),
+                        ('test_animal2', 3, 3),
+                        ('test_animal2', 0, 0)])
+
+def test_aging(test_animal, num_years, expected):
+    animal = Herbivores(test_animal)
+    for _ in range(num_years):
+        animal.ages()
+    assert animal.age == expected
+"""
+"""
 def test_herbivores_creation():
-    """
+    
     Creation has age set to 0
-    """
+    
     herb = Herbivores(test_animal)
     assert herb.age == test_animal['age']
 
@@ -24,11 +45,10 @@ def test_herbivores_creation():
                                                                (40.0, 3, 0.165906),
                                                                (30, 25, 0.815553)])
 def test_fitness_flux(age_p, weight_p, expected_fitness):
-    """
+    
     Testing the fitness function with the formula.
     If both values (?) at 0.50 in value the result should be 0.25
-    """
-
+    
     herb = Herbivores(test_animal)
     herb.age = age_p
     herb.weight = weight_p
@@ -37,10 +57,10 @@ def test_fitness_flux(age_p, weight_p, expected_fitness):
 
 
 def test_certain_death():
-    """
+    
     test_certain_death has omega=1 creating certain death for the herbivores
     since the fitness = 0 at creation
-    """
+    
     herb = Herbivores(test_animal)
     herb.death(omega=1)
     herb.fitness = 0
@@ -48,11 +68,12 @@ def test_certain_death():
 
 
 def test_weight_loss():
-    """
+    
     test_weight_loss has set weight and eta parameters
     Checks if it gives right return on eight after calculations
-    """
+    
     herb = Herbivores(test_animal)
     initial_weight = herb.weight
     herb.weight_loss()
     assert herb.weight == initial_weight - initial_weight * Herbivores.param['eta']
+"""
