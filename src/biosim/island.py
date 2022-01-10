c8109cb555dbd61f0eefd045602eeb2f5f3797cd
Ivar Holmlund <ivholmlu@nmbu.no>
2022-01-10 13:37:38 +0100

    Changed if test comparison and added t_geogr as attribute to Island

--- src/biosim/island.py
+++ src/biosim/island.py
@@ -5,18 +5,18 @@ import itertools
 class Island():
     def __init__(self, geogr=None):
         self.geogr = geogr.splitlines()
+        self.t_geogr = geogr.replace('\n', '')
         x = len(self.geogr)
         y = len(self.geogr[0])
-        length = int(len(self.geogr)**0.5)
         self.cells = {(row, column): None for row in range(1, x + 1) for column in range(1, y + 1)}
 
     def assign(self):
-        for cell, geogr in zip(self.cells, self.geogr):
-            if geogr is 'L':
+        for cell, geogr in zip(self.cells, self.t_geogr):
+            if geogr == 'L':
                 self.cells[cell] = Lowland()
-            elif geogr is 'H':
+            elif geogr == 'H':
                 self.cells[cell] = Highland()
-            elif geogr is 'D':
+            elif geogr == 'D':
                 self.cells[cell] = Desert()
             else:
                 self.cells[cell] = Water()
