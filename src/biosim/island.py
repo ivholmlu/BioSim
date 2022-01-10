6841c77b05add3e7dc8e16f9becc5d74f4c160af
Aaron De Leyos <aaron.leyds.mangaron.de.leyos@nmbu.no>
2022-01-10 11:27:24 +0100

    Added additional methods to the island class

--- src/biosim/island.py
+++ src/biosim/island.py
@@ -1,18 +1,47 @@
 from .landscape import Lowland, Highland, Desert, Water
+import textwrap
+from math import sqrt
 
 class Island():
-    def __init__(self, list_of_animals=None):
-        self.tiles = Lowland(list_of_animals)
+    def __init__(self, geogr=None):
+        self.geogr = geogr.replace('\n', '')
+        length = int(len(self.geogr)**0.5)
+        self.cells = {(x, y): None for x in range(1, length + 1) for y in range(1, length + 1)}
+
+    def assign(self):
+        for cell, geogr in zip(self.cells, self.geogr):
+            if geogr is 'L':
+                self.cells[cell] = Lowland()
+            elif geogr is 'H':
+                self.cells[cell] = Highland()
+            elif geogr is 'D':
+                self.cells[cell] = Desert()
+            else:
+                self.cells[cell] = Water()
+
+    def assign_animal(self, list_of_animals=None):
+        coord = list_of_animals[0]['loc']
+        pop = list_of_animals[0]['pop']
+        self.cells[coord].append_population(pop)
+
+
+
+
+
+
+
+
 
     # Én syklus for ett landområde
     def cycle(self):
         # Kan videre forbedres ved å implementere .sort_fitness() i hver av funksjonene
-        self.tiles.init_fitness()
-        self.tiles.sort_fitness()
-        self.tiles.feed()
-        self.tiles.procreate()
-        self.tiles.aging()
-        self.tiles.weight_cut()
-        self.tiles.deceased()
-        self.tiles.replenish()
+        self.cells.replenish()
+        self.cells.init_fitness()
+        self.cells.sort_fitness()
+        self.cells.feed()
+        self.cells.procreate()
+        self.cells.aging()
+        self.cells.weight_cut()
+        self.cells.deceased()
+
 
