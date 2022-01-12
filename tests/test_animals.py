741c7351df82e09046962e6f21d045dcba09ac7d
Ivar Holmlund <ivholmlu@nmbu.no>
2022-01-12 14:50:31 +0100

    Rewrote test_fitness_flux with better parameters for a better looking test

--- tests/test_animals.py
+++ tests/test_animals.py
@@ -56,51 +56,25 @@ class Test_creation:
         self.carn.weight_loss()
         assert self.carn.weight == expected
 
-@pytest.mark.parametrize('age_p, weight_p, expected_fitness', [(40.0, 10.0, 0.250),
-                                                               (40.0, 3, 0.165906),
-                                                               (30, 25, 0.815553)])
-def test_fitness_flux(age_p, weight_p, expected_fitness):
-
-    herb = Herbivores(test_animal1)
-    herb.age = age_p
-    herb.weight = weight_p
+@pytest.mark.parametrize('expected_fitness, weigth_age_parameters'
+                                                            , [(0.250,
+                                                                {'age': 40, 'weight':10}),
+                                                               (0.165906,
+                                                                {'age': 40, 'weight':3}),
+                                                               (0.815553,
+                                                                {'age': 30, 'weight':25})])
+def test_fitness_flux(expected_fitness, weigth_age_parameters):
+    """
+    Tests if the fitness_flux function calculates right values.
+    40 and 10 are the 'a_half' and 'w_half' parameters. If inserted for herbivores
+    they will return 0.250
+    The others are values calculated by Ivar using CAS from Geogebra and the formula
+    """
+    herb = Herbivores(weigth_age_parameters)
     herb.fitness_flux()
     assert herb.fitness == pytest.approx(expected_fitness)
 
 """
-@pytest.mark.parametrize("weight, gain",
-                        [(0, 10),
-                         (20, 50),
-                         (22, 10)])
-class Test_weight:
-
-    def test_weight_gain_Herbivores(self, weight, gain):
-        test_animal = {'age' : 0, 'weight' : weight, 'species' : 'Herbivores'}
-        animal = Herbivores(test_animal)
-        animal.weight_gain(gain)
-        assert animal.weight == weight + gain * animal.param['beta']
-
-    def test_weight_gain_Carnivores(self, weight, gain):
-        test_animal = {'age' : 0, 'weight' : weight, 'species' : 'Herbivores'}
-        animal = Carnivores(test_animal)
-        animal.weight_gain(gain)
-        assert animal.weight == weight + gain * animal.param['beta']
-
-    def test_weight_loss_Herbivores(self, weight, gain):
-        test_animal = {'age': 0, 'weight': weight, 'species': 'Herbivores'}
-        animal = Herbivores(test_animal)
-        animal.weight_loss()
-        assert animal.weight == weight - weight * animal.param['eta']
-
-    def test_weight_loss_Carnivores(self, weight, gain):
-        test_animal = {'age': 0, 'weight': weight, 'species': 'Carnivores'}
-        animal = Carnivores(test_animal)
-        animal.weight_loss()
-        assert animal.weight == weight - weight * animal.param['eta']
-
-
-
-
 
 @pytest.mark.parametrize("age, num_years, expected_age",
                          [(40, 1, 41),
