ad2334e59a8de4bc67a782980e6c064e52277026
Aaron De Leyos <aaron.leyds.mangaron.de.leyos@nmbu.no>
2022-01-13 14:28:39 +0100

    Changed variable names to make them more informative. Added a 2nd for loop after the first nested one in the cycle method

--- src/biosim/island.py
+++ src/biosim/island.py
@@ -7,17 +7,17 @@ class Island():
     def __init__(self, geogr=None):
         self.geogr = geogr.splitlines()
         self.t_geogr = geogr.replace('\n', '')
-        x = len(self.geogr)
-        y = len(self.geogr[0])
-        self.cells = {(row, column): None for row in range(1, x + 1) for column in range(1, y + 1)}
+        n_rows = len(self.geogr)
+        m_columns = len(self.geogr[0])
+        self.cells = {(row, column): None for row in range(1, n_rows + 1) for column in range(1, m_columns + 1)}
 
     def assign(self):
-        for coord, geogr in zip(self.cells, self.t_geogr):
-            if geogr == 'L':
+        for coord, landscape in zip(self.cells, self.t_geogr):
+            if landscape == 'L':
                 self.cells[coord] = Lowland()
-            elif geogr == 'H':
+            elif landscape == 'H':
                 self.cells[coord] = Highland()
-            elif geogr == 'D':
+            elif landscape == 'D':
                 self.cells[coord] = Desert()
             else:
                 self.cells[coord] = Water()
@@ -28,12 +28,13 @@ class Island():
         self.cells[coord].append_population(pop)
 
     def migrant_move(self):
-        for coord in self.cells.keys(): # Creates list over neighbour cells.
-            coord = list(coord)
-            neighbour_cell = [(coord[0] + 1, coord[1]), (coord[0], coord[1] + 1),
-                              (coord[0] - 1, coord[1]), (coord[0], coord[1] - 1)]
-            coord = tuple(coord)
-            emigrants = self.cells[coord].emigrants()
+        # Idé: Kanskje framfor å gå gjennom keys, heller gå gjennom .items slik at vi får ut både koordinat
+        # og landtype!
+        for coord, land in self.cells.items(): # Creates list over neighbour cells.
+            current_cord = list(coord)
+            neighbour_cell = [(current_cord[0] + 1, current_cord[1]), (current_cord[0], current_cord[1] + 1),
+                              (current_cord[0] - 1, current_cord[1]), (current_cord[0], current_cord[1] - 1)]
+            emigrants = land.emigrants()
             for animal in emigrants:
                 new_loc = random.choice(neighbour_cell)
                 if self.cells[new_loc].habitable is True:
@@ -41,20 +42,17 @@ class Island():
                 else:
                     self.cells[coord].stay(animal)
 
-    # Én syklus for ett landområde
-    def cycle1(self, n=1):
-        # Kan videre forbedres ved å implementere .sort_fitness() i hver av funksjonene
+    def cycle(self, n=1):
+        for _ in range(n):
+            for land in self.cells.values():
+                land.replenish()
+                land.calculate_fitness()
+                land.sort_fitness()
+                land.feed()
+                land.procreate()
+                land.migrant_move()
+                land.aging_and_weight_loss()
+                land.deceased()
 
-        for coord in self.cells.keys():
-            current_cell = self.cells[coord]
-            current_cell.replenish()
-            current_cell.init_fitness()
-            current_cell.sort_fitness()
-            current_cell.feed()
-            current_cell.procreate()
-            current_cell.migrant_move()
-            current_cell.aging()
-            current_cell.weight_cut()
-            current_cell.deceased()
-
-
+            for land in self.cells.values():
+                land.add_migrants()
