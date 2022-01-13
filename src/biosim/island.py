5636218c37401cc1c2881e164ff26a8bea012223
Aaron De Leyos <aaron.leyds.mangaron.de.leyos@nmbu.no>
2022-01-13 11:10:27 +0100

    Changed variable name cell to coord for better understanding.

--- src/biosim/island.py
+++ src/biosim/island.py
@@ -12,15 +12,15 @@ class Island():
         self.cells = {(row, column): None for row in range(1, x + 1) for column in range(1, y + 1)}
 
     def assign(self):
-        for cell, geogr in zip(self.cells, self.t_geogr):
+        for coord, geogr in zip(self.cells, self.t_geogr):
             if geogr == 'L':
-                self.cells[cell] = Lowland()
+                self.cells[coord] = Lowland()
             elif geogr == 'H':
-                self.cells[cell] = Highland()
+                self.cells[coord] = Highland()
             elif geogr == 'D':
-                self.cells[cell] = Desert()
+                self.cells[coord] = Desert()
             else:
-                self.cells[cell] = Water()
+                self.cells[coord] = Water()
 
     def assign_animals(self, list_of_animals=None):
         coord = list_of_animals[0]['loc']
@@ -28,49 +28,31 @@ class Island():
         self.cells[coord].append_population(pop)
 
     def migrant_move(self):
-        for cell in self.cells.keys(): #Creates list over neighbour cells.
-            cell = list(cell)
-            neighbour_cell = [(cell[0] + 1, cell[1]), (cell[0], cell[1] + 1),
-                              (cell[0] - 1, cell[1]), (cell[0], cell[1] - 1)]
-            cell = tuple(cell)
-            emigrants = self.cells[cell].emigrants()
+        for coord in self.cells.keys(): # Creates list over neighbour cells.
+            coord = list(coord)
+            neighbour_cell = [(coord[0] + 1, coord[1]), (coord[0], coord[1] + 1),
+                              (coord[0] - 1, coord[1]), (coord[0], coord[1] - 1)]
+            coord = tuple(coord)
+            emigrants = self.cells[coord].emigrants()
             for animal in emigrants:
                 new_loc = random.choice(neighbour_cell)
                 if self.cells[new_loc].habitable is True:
                     self.cells[new_loc].insert_migrant(animal)
                 else:
-                    self.cells[cell].stay(animal)
-
-
-
-
-
-
-
-
-
-
-
+                    self.cells[coord].stay(animal)
 
     # Én syklus for ett landområde
-    def cycle1(self, n=None):
+    def cycle1(self, n=1):
         # Kan videre forbedres ved å implementere .sort_fitness() i hver av funksjonene
-        for cell in self.cells.keys():
-            current_cell = self.cells[cell]
 
+        for coord in self.cells.keys():
+            current_cell = self.cells[coord]
             current_cell.replenish()
             current_cell.init_fitness()
             current_cell.sort_fitness()
             current_cell.feed()
             current_cell.procreate()
-            # current_cell.emigrate()
-
-    def cycle2(self, n=None):
-        # Kan videre forbedres ved å implementere .sort_fitness() i hver av funksjonene
-        for cell in self.cells.keys():
-            current_cell = self.cells[cell]
-
-            # current_cell.migrate()
+            current_cell.migrant_move()
             current_cell.aging()
             current_cell.weight_cut()
             current_cell.deceased()
