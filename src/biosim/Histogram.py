c6638bcb9c15b49de5d61289d1d052b4ae53e966
Ivar Holmlund <ivholmlu@nmbu.no>
2022-01-14 13:52:14 +0100

    Aded update to simulation

--- src/biosim/Histogram.py
+++ src/biosim/Histogram.py
@@ -1,4 +1,20 @@
 """
 File to incoorperate histogram plotting in simulation.simulate
 """
+import numpy as np
+import matplotlib.pyplot as plt
 
+def update(n_year, year):
+
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    ax.set_xlim(0, n_year)
+    ax.set_ylim(0, 1)
+
+    line = ax.plot(np.arange(n_year),
+                   np.full(n_year, np.nan), 'b-')[0]
+
+    ydata = line.get_ydata()
+    ydata[year] = np.random.random()
+    line.set_ydata(ydata)
+    plt.pause(1e-6)
