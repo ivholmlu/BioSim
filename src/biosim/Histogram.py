41e8f09d84cbe61dfd2def3bb96bc90b658902ee
Ivar Holmlund <ivholmlu@nmbu.no>
2022-01-14 14:06:21 +0100

    Testing with Histogram as class

--- src/biosim/Histogram.py
+++ src/biosim/Histogram.py
@@ -4,17 +4,23 @@ File to incoorperate histogram plotting in simulation.simulate
 import numpy as np
 import matplotlib.pyplot as plt
 
-def update(n_year, year):
+class Histogram:
 
-    fig = plt.figure()
-    ax = fig.add_subplot(1, 1, 1)
-    ax.set_xlim(0, n_year)
-    ax.set_ylim(0, 1)
+    def __init__(self):
+        self.line = None
 
-    line = ax.plot(np.arange(n_year),
-                   np.full(n_year, np.nan), 'b-')[0]
+    def setup(self):
+        fig = plt.figure()
+        ax = fig.add_subplot(1, 1, 1)
+        ax.set_xlim(0, n_year)
+        ax.set_ylim(0, 1)
 
-    ydata = line.get_ydata()
-    ydata[year] = np.random.random()
-    line.set_ydata(ydata)
-    plt.pause(1e-6)
+        self.line = ax.plot(np.arange(n_year),
+                       np.full(n_year, np.nan), 'b-')[0]
+
+    def update(self, year, Herbivore):
+
+        ydata = self.line.get_ydata()
+        ydata[year] = Herbivore
+        self.line.set_ydata(ydata)
+        plt.pause(1e-6)
