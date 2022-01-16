9a0758627f1558159ff1c2b467b884041f98fcb6
Aaron De Leyos <aaron.leyds.mangaron.de.leyos@nmbu.no>
2022-01-16 12:06:52 +0100

    Changed the Herbivores' DeltaPhiMax parameter to 0 as it was conflicting with our set_params class method.

--- src/biosim/animals.py
+++ src/biosim/animals.py
@@ -80,8 +80,9 @@ class Animals:
                 cls.param[key] = new_params[key]
 
             if 'DeltaPhiMax' in new_params:
-                if not new_params['DeltaPhiMax'] > 0:
-                    raise ValueError('DeltaPhiMax must be strictly greater than 0.')
+                if type(cls) is Carnivores:
+                    if not new_params['DeltaPhiMax'] > 0:
+                        raise ValueError('DeltaPhiMax must be strictly greater than 0.')
 
             if 'eta' in new_params:
                 if not new_params['eta'] <= 1:
@@ -98,7 +99,7 @@ class Herbivores(Animals):
              'eta': 0.05, 'a_half': 40.0, 'phi_age': 0.6,
              'w_half': 10.0, 'phi_weight': 0.1, 'mu': 0.25,
              'gamma': 0.2, 'zeta': 3.5, 'xi': 1.2,
-             'omega': 0.4, 'F': 10.0, 'DeltaPhiMax': None}
+             'omega': 0.4, 'F': 10.0, 'DeltaPhiMax': 0.0}
 
 
 class Carnivores(Animals):
