b596ee1c6f4f210b76e83ea264ba7f37b0873b4d
Aaron De Leyos <aaron.leyds.mangaron.de.leyos@nmbu.no>
2022-01-16 13:04:31 +0100

    Added additional tests.

--- tests/test_landscape.py
+++ tests/test_landscape.py
@@ -7,16 +7,25 @@ import pytest
 import numpy as np
 
 
-def test_lowland_create():
+def test_create_landscapes():
     lowland = Lowland()
-    assert lowland.current_fodder == 0  # The current fodder should always be set to 0
+    highland = Highland()
+    desert = Desert()
+    water = Water()
+    assert lowland.current_fodder == 0 and highland.current_fodder == 0\
+           and desert.current_fodder == 0 and water.current_fodder == 0  # The current fodder should always be set to 0
     # regardless of the landscape type. Only when .replenish() is
-    # called, shall .current_fodder == f_max
+    # called, should .current_fodder == f_max
 
+@pytest.mark.parametrize('f_max', [100, 240, 700])
+def test_set_parameters(f_max):
+    highland = Highland()
+    highland.set_params({'f_max': f_max})
+    highland.replenish()
+    assert highland.f_max == highland.current_fodder
 
 n = 50
 m = 20
-
 
 @pytest.mark.parametrize('herbivores, carnivores',
                          [([{'pop': [{'species': 'Herbivore', 'age': 5, 'weight': 20} for _ in range(n)]}],
@@ -92,7 +101,7 @@ class TestPopulation:
 
     def test_procreation(self, create_animals, insert_animals, mocker):
         """
-        Test to assure that all the animals will procreate, and the population size increases.
+        Test to assure that all the animals will procreate and the population size increases.
         """
         mocker.patch('random.random', return_value=0)
         self.lowland.replenish()
@@ -112,4 +121,5 @@ class TestPopulation:
         tot_carnivores1 = len(self.lowland.carnivores)
 
         assert tot_herbivores1 > tot_herbivores0 and tot_carnivores1 > tot_carnivores0
+
 
