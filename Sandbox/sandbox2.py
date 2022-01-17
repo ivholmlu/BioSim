5d5be5ca8a24cf82a3c5a86d7a70cccdf717cba5
Aaron De Leyos <aaron.leyds.mangaron.de.leyos@nmbu.no>
2022-01-17 17:43:42 +0100

    Sandbox.

--- Sandbox/sandbox2.py
+++ Sandbox/sandbox2.py
@@ -1,80 +1,59 @@
-from biosim.landscape import Lowland, Water
-import matplotlib.pyplot as plt
+from biosim.test_simu import BioSim
+from biosim.landscape import Lowland
 from biosim.island import Island
 import textwrap
+import random
+import matplotlib.pyplot as plt
 
-ini_herbs = [{'loc': (2, 2),
-              'pop': [{'species': 'Herbivore',
-                       'age': 5,
-                       'weight': 20}
-                      for _ in range(50)]}]
-
-
-ini_carns = [{'loc': (2, 2),
-              'pop': [{'species': 'Carnivore',
-                       'age': 5,
-                       'weight': 20}
-                      for _ in range(20)]}]
-a = [1, 2, 3]
-b = [4, 5, 6]
-c = [7, 8, 9]
-
-for i in zip(a, b, c):
-    print(i)
+random.seed(15838511)
 
 geogr = """\
-           WWWW
-           WLLW
-           WWWW"""
+           WWWWWWWWWWWWWWWWWWWWW
+           WHHHHHLLLLWWLLLLLLLWW
+           WHHHHHLLLLWWLLLLLLLWW
+           WHHHHHLLLLWWLLLLLLLWW
+           WWHHLLLLLLLWWLLLLLLLW
+           WWHHLLLLLLLWWLLLLLLLW
+           WWWWWWWWHWWWWLLLLLLLW
+           WHHHHHLLLLWWLLLLLLLWW
+           WHHHHHHHHHWWLLLLLLWWW
+           WHHHHHDDDDDLLLLLLLWWW
+           WHHHHHDDDDDLLLLLLLWWW
+           WHHHHHDDDDDLLLLLLLWWW
+           WHHHHHDDDDDWWLLLLLWWW
+           WHHHHDDDDDDLLLLWWWWWW
+           WWHHHHDDDDDDLWWWWWWWW
+           WWHHHHDDDDDLLLWWWWWWW
+           WHHHHHDDDDDLLLLLLLWWW
+           WHHHHDDDDDDLLLLWWWWWW
+           WWHHHHDDDDDLLLWWWWWWW
+           WWWHHHHLLLLLLLWWWWWWW
+           WWWHHHHHHWWWWWWWWWWWW
+           WWWWWWWWWWWWWWWWWWWWW"""
 
-geogr = textwrap.dedent(geogr)
+island_map = textwrap.dedent(geogr)
 
-oy = Island(geogr)
+a = Island(island_map)
 
-oy.assign()
-oy.assign_animals(ini_herbs)
-oy.assign_animals(ini_carns)
-oy.migrant_move()
-
-
-
-
-"""
-import random
-from biosim.landscape import Lowland
-import matplotlib.pyplot as plt
-random.seed(100)
-
-ini_herbs = [{'pop': [{'species': 'Herbivore',
+ini_herbs = [{'loc': (2, 7),
+              'pop': [{'species': 'g',
                        'age': 5,
                        'weight': 20}
-                      for _ in range(50)]}]
+                      for _ in range(200)]}]
 
-a = Lowland(ini_herbs)
-a.add_population()
-years = []
-years_pop = []
-years_babies = []
-for _ in range(100):
-    a.init_fitness()
-    a.sort_fitness()
-    a.feed()
-    a.procreate()
-    a.aging()
-    a.weight_cut()
-    a.deceased()
-    a.replenish()
-    years.append(_)
-    years_pop.append(len(a.population))
-    years_babies.append(len(a.babies))
+a.assign_animals(ini_herbs)
 
-plt.plot(years, years_pop)
-plt.plot(years, years_babies)
-plt.title('Amount and birth each year')
-plt.xlabel('year')
-plt.ylabel('amount')
-plt.legend(['weight', 'birth'])
-plt.show()
-print(len(a.population))
-
-"""
+#%%
+from biosim.test_simu import BioSim
+from biosim.landscape import Lowland
+from biosim.island import Island
+import textwrap
+import random
+import matplotlib.pyplot as plt
+b = Lowland()
+ini_herbs = [{'loc': (41, 3),
+              'pop': [{'species': 'Humans',
+                       'age': 5,
+                       'weight': 20}
+                      for _ in range(200)]}]
+b.append_population(ini_herbs[0]['pop'])
