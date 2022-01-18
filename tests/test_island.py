d2c750c08c7a50a732c95fc8c8bf4e7a310d6028
Ivar Holmlund <ivholmlu@nmbu.no>
2022-01-18 08:36:09 +0100

    Fixed warning signs

--- tests/test_island.py
+++ tests/test_island.py
@@ -2,7 +2,6 @@
 Test file to test island module in biosim
 """
 import pytest
-import textwrap
 from biosim.island import Island
 from biosim.landscape import Lowland, Highland, Desert, Water
 amount_herbivores = 50
@@ -22,8 +21,7 @@ ini_carns = [{'loc': loc,
                       for _ in range(amount_carnivores)]}]
 
 
-
-class Test_island_cycle_and_creation:
+class TestIslandCycleAndCreation:
 
     @pytest.fixture(autouse=True)
     def create_island(self):
@@ -42,7 +40,7 @@ class Test_island_cycle_and_creation:
         mocker.spy(self.island, "cycle")
         for _ in range(num_years):
             self.island.cycle()
-
+        # noinspection PyUnresolvedReferences
         assert self.island.cycle.call_count == num_years
 
     def test_assign_animals(self):
@@ -55,11 +53,12 @@ class Test_island_cycle_and_creation:
 
     def test_assign(self):
         """
-        Test assign function. Checks if island.assign in create_map has
+        Test assign function. Checks if island assign function in create_map has
         assigned loc = (2,2) to its correct object.
         """
 
-        assert type(self.island.cells[(2, 2)]) == type(Desert())
+        #assert type(self.island.cells[(2, 2)]) == type(Desert())  #Hvilken er best
+        assert isinstance(self.island.cells[(2, 2)], Desert)
 
     def test_get_animals_per_species(self):
         """
@@ -75,7 +74,7 @@ class Test_island_cycle_and_creation:
         """
         self.island.assign_animals(ini_herbs)
         coord_animals = self.island.get_coord_animals()
-        assert coord_animals[(2,2)]['Herbivores'] == amount_herbivores
+        assert coord_animals[(2, 2)]['Herbivores'] == amount_herbivores
 
     def test_get_attributes(self):
         """
@@ -84,8 +83,10 @@ class Test_island_cycle_and_creation:
 
         self.island.assign_animals(ini_herbs+ini_carns)
         dict_attributes = self.island.get_attributes()
-        assert dict_attributes['Herbivores']['age'] == [5 for i in range(amount_herbivores)]
-        assert dict_attributes['Carnivores']['age'] == [5 for i in range(amount_carnivores)]
+        assert dict_attributes['Herbivores']['age'] == [5 for _ in range(amount_herbivores)]
+        assert dict_attributes['Carnivores']['age'] == [5 for _ in range(amount_carnivores)]
+
+
 def test_get_neighbours():
     """
     Test if get neighbour function returns the correct coordinates for a
@@ -93,43 +94,42 @@ def test_get_neighbours():
     """
     assert Island.get_neighbours((2, 2)) == [(3, 2), (2, 3), (1, 2), (2, 1)]
 
-@pytest.mark.parametrize('Invalid_cells', [(-3, 1), (1,1)])
-def test_assign_animals_to_invalid_cell(Invalid_cells):
+
+@pytest.mark.parametrize('invalid_cells', [(-3, 1), (1, 1)])
+def test_assign_animals_to_invalid_cell(invalid_cells):
     """
     Test that checks if ValueError are raised if animals are assigned to an inhabitable coord
-    or an invalid coord
-
-    Parameters
-    ----------
-    Invalid_cells : list of tuples
+    or an invalid coordinate
     """
-    map = 'WWW\nWLW\nWWW'
-    island = Island(map)
+    geogr = 'WWW\nWLW\nWWW'
+    island = Island(geogr)
     island.assign()
     with pytest.raises(ValueError):
-        island.assign_animals([{'loc': Invalid_cells,
+        island.assign_animals([{'loc': invalid_cells,
                                 'pop': [{'species': 'Herbivore',
                                          'age': 5,
                                          'weight': 20}
                                         for _ in range(1000)]}])
 
+
 def test_no_migration_to_water():
     """
     Test that no animals have migrated to any of the water neighbour cells
     """
-    map = 'WWW\nWLW\nWWW'
-    island = Island(map)
-    island.assign_animals([{'loc': (2,2),
-              'pop': [{'species': 'Herbivore',
-                       'age': 5,
-                       'weight': 20}
-                      for _ in range(1000)]}])
+    geogr = 'WWW\nWLW\nWWW'
+    island = Island(geogr)
+    island.assign_animals([{'loc': (2, 2),
+                          'pop': [{'species': 'Herbivore',
+                                   'age': 5,
+                                   'weight': 20}
+                                  for _ in range(1000)]}])
     island.cycle()
-    neighbours = island.get_neighbours((2,2))
+    neighbours = island.get_neighbours((2, 2))
     for neighbour in neighbours:
         assert len(island.cells[neighbour].herbivores) == 0
 
-def test_migration(mocker):
+
+def test_migration():
     """
     Test if all neighbour cell are migrated to after one cycle. Placing many animals to ensure
     migration
@@ -137,16 +137,13 @@ def test_migration(mocker):
     island = Island('WWWWW\nWLLLW\nWLLLW\nWLLLW\nWWWWW')
     island.assign()
     island.assign_animals([{'loc': (3, 3),
-              'pop': [{'species': 'Herbivore',
-                       'age': 5,
-                       'weight': 20}
-                      for _ in range(1000)]}]) #Vanskelig å implementere med mocker ettersom
-    #cycle er avhengig av flere ledd med random.random()?
+                            'pop': [{'species': 'Herbivore', 'age': 5, 'weight': 20} for _ in range(1000)]}])
 
     island.cycle()
     neighbour_cells = island.get_neighbours((3, 3))
     for cell in neighbour_cells:
         assert island.cells[cell].get_population() > 0
+
 
 @pytest.mark.parametrize('landscapes', ['D', 'L', 'H'])
 class TestMap:
@@ -162,9 +159,9 @@ class TestMap:
         landscapes : list of strings
 
         """
-        map = f'WWW\nWDW\nWW{landscapes}'
+        geogr = f'WWW\nWDW\nWW{landscapes}'
         with pytest.raises(ValueError):
-            island = Island(map)
+            Island(geogr)
 
     def test_map_boundaries_sides(self, landscapes):
         """
@@ -174,43 +171,35 @@ class TestMap:
         landscapes : list of strings
 
         """
-        map = f'WWW\nWD{landscapes}\nWWW'
+        geogr = f'WWW\nWD{landscapes}\nWWW'
         with pytest.raises(ValueError):
-            island = Island(map)
+            Island(geogr)
 
 
 def test_map_unequal_rows_or_columns():
     """
     Test if unequal lengths or columns raises ValueError
     """
-    map = 'WWW\nWDWW\nWWW'
+    geogr = 'WWW\nWDWW\nWWW'
     with pytest.raises(ValueError):
-        island = Island(map)
+        Island(geogr)
+
 
 def test_map_invalid_landscape():
     """
     Test if invalid landscape name raises ValueError
     """
-    map = 'WWW\nWXW\nWWW'
+    geogr = 'WWW\nWXW\nWWW'
     with pytest.raises(ValueError):
-        island = Island(map)
+        Island(geogr)
 
-@pytest.mark.parametrize('Landscape, symbol', [(Lowland,'L'), (Desert, 'D'), (Highland, 'H')])
-def test_right_landscape_in_cell(Landscape, symbol):
+
+@pytest.mark.parametrize('landscape, symbol', [(Lowland, 'L'), (Desert, 'D'), (Highland, 'H'), (Water, 'W')])
+def test_right_landscape_in_cell(landscape, symbol):
     """
     Test that right landscape is assigned to the right cell
     """
-    map = f'WWW\nW{symbol}W\nWWW'
-    island = Island(map)
+    geogr = f'WWW\nW{symbol}W\nWWW'
+    island = Island(geogr)
 
-    assert type(island.cells[(2,2)]) == Landscape
-
-
-
-
-
-
-
-
-
-
+    assert type(island.cells[(2, 2)]) == landscape
