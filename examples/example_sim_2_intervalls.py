dda59dde47cc5c23cfd2af1126fd03370e156799
Aaron De Leyos <aaron.leyds.mangaron.de.leyos@nmbu.no>
2022-01-18 13:40:09 +0100

    Added examples on how the code is being used.

---
+++ examples/example_sim_2_intervalls.py
@@ -0,0 +1,47 @@
+"""
+Example of a simulation with two intervals, visualizing the data every 5 years.
+"""
+from biosim.simulation import BioSim
+import textwrap
+
+geogr = """\
+           WWWWWWWWWWWWWWWWWWWWW
+           WHHHHHLLLLWWLLLLLLLWW
+           WHHHHHLLLLWWLLLLLLLWW
+           WHHHHHLLLLWWLLLLLLLWW
+           WWHHLLLLLLLWWLLLLLLLW
+           WWHHLLLLLLLWWLLLLLLLW
+           WWWWWWWWHWWWWLLLLLLLW
+           WHHHHHLLLLWWLLLLLLLWW
+           WHHHHHHHHHWWLLLLLLWWW
+           WHHHHHDDDDDLLLLLLLWWW
+           WHHHHHDDDDDLLLLLLLWWW
+           WHHHHHDDDDDLLLLLLLWWW
+           WHHHHHDDDDDWWLLLLLWWW
+           WHHHHDDDDDDLLLLWWWWWW
+           WWHHHHDDDDDDLWWWWWWWW
+           WWHHHHDDDDDLLLWWWWWWW
+           WHHHHHDDDDDLLLLLLLWWW
+           WHHHHDDDDDDLLLLWWWWWW
+           WWHHHHDDDDDLLLWWWWWWW
+           WWWHHHHLLLLLLLWWWWWWW
+           WWWHHHHHHWWWWWWWWWWWW
+           WWWWWWWWWWWWWWWWWWWWW"""
+
+island_map = textwrap.dedent(geogr)
+
+ini_herbs = [{'loc': (2, 7),
+              'pop': [{'species': 'Herbivore',
+                       'age': 5,
+                       'weight': 20}
+                      for _ in range(300)]}]
+ini_carns = [{'loc': (2, 7),
+              'pop': [{'species': 'Carnivore',
+                       'age': 5,
+                       'weight': 25}
+                      for _ in range(100)]}]
+
+sim = BioSim(island_map, ini_herbs, seed=15838511, vis_years=5)
+sim.simulate(51)
+sim.add_population(ini_carns)
+sim.simulate(51)
---
+++ examples/example_write_log_sim.py
@@ -0,0 +1,45 @@
+"""
+Example of a simulation without visualization and also writing data to a csv file.
+"""
+from biosim.simulation import BioSim
+import textwrap
+
+geogr = """\
+           WWWWWWWWWWWWWWWWWWWWW
+           WHHHHHLLLLWWLLLLLLLWW
+           WHHHHHLLLLWWLLLLLLLWW
+           WHHHHHLLLLWWLLLLLLLWW
+           WWHHLLLLLLLWWLLLLLLLW
+           WWHHLLLLLLLWWLLLLLLLW
+           WWWWWWWWHWWWWLLLLLLLW
+           WHHHHHLLLLWWLLLLLLLWW
+           WHHHHHHHHHWWLLLLLLWWW
+           WHHHHHDDDDDLLLLLLLWWW
+           WHHHHHDDDDDLLLLLLLWWW
+           WHHHHHDDDDDLLLLLLLWWW
+           WHHHHHDDDDDWWLLLLLWWW
+           WHHHHDDDDDDLLLLWWWWWW
+           WWHHHHDDDDDDLWWWWWWWW
+           WWHHHHDDDDDLLLWWWWWWW
+           WHHHHHDDDDDLLLLLLLWWW
+           WHHHHDDDDDDLLLLWWWWWW
+           WWHHHHDDDDDLLLWWWWWWW
+           WWWHHHHLLLLLLLWWWWWWW
+           WWWHHHHHHWWWWWWWWWWWW
+           WWWWWWWWWWWWWWWWWWWWW"""
+
+island_map = textwrap.dedent(geogr)
+
+ini_herbs = [{'loc': (2, 7),
+              'pop': [{'species': 'Herbivore',
+                       'age': 5,
+                       'weight': 20}
+                      for _ in range(300)]}]
+ini_carns = [{'loc': (2, 7),
+              'pop': [{'species': 'Carnivore',
+                       'age': 5,
+                       'weight': 25}
+                      for _ in range(100)]}]
+
+sim = BioSim(island_map, ini_herbs+ini_carns, seed=15838511, vis_years=0, log_file='sim_data')
+sim.simulate(201)
