3a076f279740ec9320954a0888d58fa59da55464
Ivar Holmlund <ivholmlu@nmbu.no>
2022-01-18 11:20:48 +0100

    Added test to simulation

--- tests/test_simulation.py
+++ tests/test_simulation.py
@@ -26,13 +26,26 @@ ini_carns = [{'loc': (2, 3),
                       for _ in range(50)]}]
 
 num_years = 10
+seed = 100
+
 def test_simulation():
     """
     Test that Biosim runs without problems
     Runs the simulation for num_years
     """
-    sim = BioSim(geogr, ini_herbs + ini_carns, vis_years=10)
+    sim = BioSim(geogr, ini_herbs + ini_carns, seed=seed, vis_years=10)
     sim.simulate(num_years)
+
+def test_no_plot_simulation():
+    """
+    Test to check that the plot window does not appear when vis_years is set to 0.
+    """
+    sim = BioSim(geogr, ini_herbs, seed=seed, vis_years=0)
+    sim.simulate(50)
+    sim.add_population(ini_carns)
+    sim.simulate(50)
+
+
 
 
 @pytest.mark.parametrize('landscape, f_max',[('H', {'f_max': 100}), ('L', {'f_max': 300}), ('D', {'f_max': 40})])
