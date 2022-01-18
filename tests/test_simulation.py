1e5c31af65eb39f3b6ceeb2a1c22d66d78e90a3e
Ivar Holmlund <ivholmlu@nmbu.no>
2022-01-18 10:57:11 +0100

    Changed island in test_simu and som parameters for Biosim

--- tests/test_simulation.py
+++ tests/test_simulation.py
@@ -2,14 +2,22 @@
 Test file to test simulation for biosim
 """
 from biosim.simulation import BioSim
+import textwrap
+geogr = """\
+           WWWWW
+           WLLLW
+           WLHLW
+           WDDHW
+           WWWWW"""
 
-geogr = 'WWWWWWWWWWWWWWWWWWWWW\nWHHHHHLLLLWWLLLLLLLWW\nWHHHHHLLLLWWLLLLLLLWW\nWHHHHHLLLLWWLLLLLLLWW\nWWHHLLLLLLLWWLLLLLLLW\nWWHHLLLLLLLWWLLLLLLLW\nWWWWWWWWHWWWWLLLLLLLW\nWHHHHHLLLLWWLLLLLLLWW\nWHHHHHHHHHWWLLLLLLWWW\nWHHHHHDDDDDLLLLLLLWWW\nWHHHHHDDDDDLLLLLLLWWW\nWHHHHHDDDDDLLLLLLLWWW\nWHHHHHDDDDDWWLLLLLWWW\nWHHHHDDDDDDLLLLWWWWWW\nWWHHHHDDDDDDLWWWWWWWW\nWWHHHHDDDDDLLLWWWWWWW\nWHHHHHDDDDDLLLLLLLWWW\nWHHHHDDDDDDLLLLWWWWWW\nWWHHHHDDDDDLLLWWWWWWW\nWWWHHHHLLLLLLLWWWWWWW\nWWWHHHHHHWWWWWWWWWWWW\nWWWWWWWWWWWWWWWWWWWWW'
-ini_herbs = [{'loc': (2, 7),
+geogr = textwrap.dedent(geogr)
+
+ini_herbs = [{'loc': (2, 2),
               'pop': [{'species': 'Herbivore',
                        'age': 5,
                        'weight': 20}
                       for _ in range(200)]}]
-ini_carns = [{'loc': (2, 7),
+ini_carns = [{'loc': (2, 3),
               'pop': [{'species': 'Carnivore',
                        'age': 5,
                        'weight': 20}
@@ -20,9 +28,6 @@ def test_simulation():
     """
     Test that Biosim runs without problems
     Runs the simulation for num_years
-    Returns
-    -------
-
     """
     sim = BioSim(geogr, ini_herbs + ini_carns, seed=100, vis_years=10)
     sim.simulate(num_years)
